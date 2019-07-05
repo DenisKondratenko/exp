f7064cd6c2971eef962328c07e30ded30204f258
DenisKondratenko <homiekondratenko@gmail.com>
2019-07-05 13:41:53 +0300

    exp 14.1

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
-	res.send('this is home');
+	res.sendFile(__dirname + "/index.html");
 });
 
 app.get('/news/', function(req, res) {
 	res.send('This is news');
 });
-app.get('/news/:name/:id', function(req, res) {
-	res.send('This is news ID -'+ req.params.name + req.params.id);
+
+
+
+app.get('/news/:id', function(req, res) {
+	res.render('news', {newsId: req.params.id});
 });
 app.listen(3000);
 
